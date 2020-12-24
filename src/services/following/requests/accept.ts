abeff9e0990ab97c5d05f746e08c66560206ee3c
mei23 <m@m544.net>
2020-12-24 13:32:09 +0900

    フォローリクエストがなくてもフォロー承認が出来てしまうのを修正

--- src/services/following/requests/accept.ts
+++ src/services/following/requests/accept.ts
@@ -6,6 +6,7 @@ import renderAccept from '../../../remote/activitypub/renderer/accept';
 import { deliver } from '../../../queue';
 import { publishMainStream } from '../../stream';
 import { insertFollowingDoc } from '../create';
+import { IdentifiableError } from '../../../misc/identifiable-error';
 
 export default async function(followee: IUser, follower: IUser) {
 	const request = await FollowRequest.findOne({
@@ -13,9 +14,13 @@ export default async function(followee: IUser, follower: IUser) {
 		followerId: follower._id
 	});
 
+	if (request == null) {
+		throw new IdentifiableError('8884c2dd-5795-4ac9-b27e-6a01d38190f9', 'No follow request.');
+	}
+
 	await insertFollowingDoc(followee, follower);
 
-	if (isRemoteUser(follower) && request) {
+	if (isRemoteUser(follower)) {
 		const content = renderActivity(renderAccept(renderFollow(follower, followee, request.requestId), followee as ILocalUser));
 		deliver(followee as ILocalUser, content, follower.inbox);
 	}
