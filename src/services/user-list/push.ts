8cd52e473bc268ea2d76b89b752d965e5f134944
mei23 <m@m544.net>
2020-12-27 04:15:15 +0900

    その人がフォロー申請中ならプロキシアカウントがフォローしないようにする Resolve #2039

--- src/services/user-list/push.ts
+++ src/services/user-list/push.ts
@@ -1,9 +1,11 @@
-import { pack as packUser, IUser, isRemoteUser, fetchProxyAccount } from '../../models/user';
+import * as mongo from 'mongodb';
+import { pack as packUser, IUser, isRemoteUser, fetchProxyAccount, IRemoteUser, ILocalUser } from '../../models/user';
 import UserList, { IUserList } from '../../models/user-list';
 import { publishUserListStream } from '../stream';
 import Following from '../../models/following';
 import { fetchOutbox } from '../../remote/activitypub/models/person';
 import createFollowing from '../following/create';
+import FollowRequest from '../../models/follow-request';
 
 export async function pushUserToUserList(target: IUser, list: IUserList) {
 	await UserList.update({ _id: list._id }, {
@@ -14,18 +16,33 @@ export async function pushUserToUserList(target: IUser, list: IUserList) {
 
 	publishUserListStream(list._id, 'userAdded', await packUser(target));
 
+	fetchOutbox(target);
+
 	// このインスタンス内にこのリモートユーザーをフォローしているユーザーがいなくても投稿を受け取るためにダミーのユーザーがフォローしたということにする
 	if (isRemoteUser(target)) {
-		const count = await Following.count({
-			followeeId: target._id,
-			'_follower.host': null
-		});
-
-		if (count < 1) {
-			const proxy = await fetchProxyAccount();
-			createFollowing(proxy, target);
-
-			fetchOutbox(target);
-		}
+		tryProxyFollow(target, list.userId);
 	}
+}
+
+async function tryProxyFollow(target: IRemoteUser, userId: mongo.ObjectID) {
+	const count = await Following.count({
+		followeeId: target._id,
+		'_follower.host': null
+	});
+
+	if (count > 0) {
+		return;
+	}
+
+	const req = await FollowRequest.findOne({
+		followerId: userId,
+		followeeId: target._id
+	});
+
+	if (req) {
+		return;
+	}
+
+	const proxy = await fetchProxyAccount();
+	createFollowing(proxy, target);
 }
