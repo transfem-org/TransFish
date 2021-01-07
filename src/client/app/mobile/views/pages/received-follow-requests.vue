5125b0c820d9fa6edb63e9cbf94915e42407ea55
mei23 <m@m544.net>
2021-01-07 22:11:02 +0900

    フォロー申請画面もうちょっといい感じに Resolve #2108

--- src/client/app/mobile/views/pages/received-follow-requests.vue
+++ src/client/app/mobile/views/pages/received-follow-requests.vue
@@ -3,13 +3,17 @@
 	<template #header><fa :icon="['far', 'envelope']"/>{{ $t('title') }}</template>
 
 	<main>
-		<div v-for="req in requests">
-			<router-link :key="req.id" :to="req.follower | userPage">
-				<mk-user-name :user="req.follower"/>
-			</router-link>
-			<span>
-				<a @click="accept(req.follower)">{{ $t('accept') }}</a>|<a @click="reject(req.follower)">{{ $t('reject') }}</a>
-			</span>
+		<div class="user" v-for="req in requests" :key="req.id">
+			<mk-avatar class="avatar" :user="req.follower"/>
+			<div class="body">
+				<div class="name">
+					<router-link class="name" :to="req.follower | userPage" v-user-preview="req.follower.id"><mk-user-name :user="req.follower"/></router-link>
+					<p class="username">@{{ req.follower | acct }}</p>
+				</div>
+			</div>
+			<div class="action">
+				<a @click="accept(req.follower)">{{ $t('accept') }}</a> | <a @click="reject(req.follower)">{{ $t('reject') }}</a>
+			</div>
 		</div>
 	</main>
 </mk-ui>
@@ -57,13 +61,43 @@ export default Vue.extend({
 
 <style lang="stylus" scoped>
 main
-	> div
+	color: var(--text)
+
+	> .user
 		display flex
 		padding 16px
-		border solid 1px var(--faceDivider)
-		border-radius 4px
+		border-bottom solid 1px var(--faceDivider)
+		align-items center
 
-		> span
+		&:last-child
+			border-bottom none
+
+		> .avatar
+			display block
+			flex-shrink 0
+			margin 0 12px 0 0
+			width 42px
+			height 42px
+			border-radius 8px
+
+		> .body
+			> .name
+				> .name
+					margin 0
+					font-size 16px
+					line-height 24px
+					color var(--text)
+
+				> .username
+					display block
+					margin 0
+					font-size 13px
+					line-height 13px
+					color var(--text)
+					opacity 0.7
+
+		> .action
 			margin 0 0 0 auto
-
+			> a
+				padding 0 0.3em
 </style>
