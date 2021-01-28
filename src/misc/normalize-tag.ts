602af1ab747d582c1e94efa1e482e551a6236bba
mei23 <m@m544.net>
2021-01-28 22:50:15 +0900

    refactor

--- src/misc/normalize-tag.ts
+++ src/misc/normalize-tag.ts
@@ -1,4 +1,4 @@
-export function normalizeTag(tag?: string | null): string | null | undefined {
+export function normalizeTag(tag: string | null | undefined): string | null | undefined {
 	if (tag == null) return tag;
 	return tag.normalize('NFKC').toLowerCase();
 }
