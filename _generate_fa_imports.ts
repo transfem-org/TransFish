f155478f82f60ae37e13a8bc7b7324ecffb8f0c1
mei23 <m@m544.net>
2021-04-23 07:50:52 +0900

    Squashed commit of the following:
    commit d9aa9728a523860cc20ca41feda34056afda4308
    Author: mei23 <m@m544.net>
    Date:   Fri Apr 23 07:41:19 2021 +0900
    
        Format fa import

---
+++ _generate_fa_imports.ts
@@ -0,0 +1,79 @@
+import * as fs from 'fs';
+//import { inspect } from 'util';
+import { concat } from './src/prelude/array';
+
+
+// TODO: unique, auto generate r/s
+const importRegex = new RegExp(/import\s+\{([^}]+)\}\s+from\s+'@fortawesome\/free-(\w+)-svg-icons';/g, 'gs');
+
+function main() {
+	const content = fs.readFileSync('src/client/app/init.ts', 'utf-8');
+
+	const fais = getFaImports(content);
+
+	const toAddLib: string[][] = [];
+
+	for (const fai of fais) {
+		console.log(`// Imports for Font Awesome ${fai.name}`)
+		console.log(formatFaImport(fai));
+
+		toAddLib.push(fai.imports.map(x => x.as || x.name));
+	}
+
+	console.log(`library.add(\n\t${concat(toAddLib).join(',\n\t')}\n);`);
+}
+
+function getFaImports(s: string): FaImport[] {
+	const faImports: FaImport[] = [];
+
+	let a1: RegExpExecArray | null
+	while ((a1 = importRegex.exec(s)) !== null) {
+		const name = a1[2];
+		const imp = a1[1];
+		const ps = splitComma(imp);
+		const imports = ps.map(p => parseImport(p));
+
+		const fai = {
+			name,
+			imports: sortImportsByName(imports)
+		} as FaImport;
+
+		faImports.push(fai);
+	}
+
+	return faImports;
+}
+
+function formatFaImport(fai: FaImport): string {
+	const imports = fai.imports.map(f => f.as ? `${f.name} as ${f.as}` : f.name).join(',\n\t');
+	return `import {\n\t${imports}\n} from '@fortawesome/free-${fai.name}-svg-icons';`
+}
+
+function splitComma(s: string): string[] {
+	return s.trim().split(/[\s\n]*,[\s\n]*/).filter(x => x);
+}
+
+type FaImport = {
+	name: string;
+	imports: Import[];
+};
+
+type Import = {
+	name: string;
+	as?: string;
+};
+
+function parseImport(s: string): Import {
+	const matchAs = s.match(/^(\w+)\s+as\s+(\w+)$/);
+	if (matchAs) {
+		return { name: matchAs[1], as: matchAs[2] }
+	} else {
+		return { name: s, as: undefined }
+	}
+}
+
+function sortImportsByName(is: Import[]): Import[] {
+	return is.sort((a, b) => a.name.localeCompare(b.name))
+}
+
+main();
