d3bca3bef2066b88b382d8e939bf702248a4f3c8
mei23 <m@m544.net>
2021-05-09 13:10:18 +0900

    Fix http-proxy-agent type issues

--- src/misc/fetch.ts
+++ src/misc/fetch.ts
@@ -91,14 +91,14 @@ const _https = new https.Agent({
  * Get http proxy or non-proxy agent
  */
 export const httpAgent = config.proxy
-	? new HttpProxyAgent(config.proxy) as unknown as http.Agent
+	? new HttpProxyAgent(config.proxy)
 	: _http;
 
 /**
  * Get https proxy or non-proxy agent
  */
 export const httpsAgent = config.proxy
-	? new HttpsProxyAgent(config.proxy) as unknown as https.Agent
+	? new HttpsProxyAgent(config.proxy)
 	: _https;
 
 /**
