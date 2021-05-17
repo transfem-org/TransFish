a06da90b195833a695bc8aff22ac7eec29259d8a
mei23 <m@m544.net>
2021-05-18 01:14:01 +0900

    null Undo が送られるのを修正

--- src/remote/activitypub/renderer/undo.ts
+++ src/remote/activitypub/renderer/undo.ts
@@ -1,8 +1,12 @@
 import config from '../../../config';
 import { ILocalUser, IUser } from '../../../models/user';
 
-export default (object: any, user: ILocalUser | IUser) => ({
-	type: 'Undo',
-	actor: `${config.url}/users/${user._id}`,
-	object
-});
+export default (object: any, user: ILocalUser | IUser) => {
+	if (object == null) return null;
+
+	return {
+		type: 'Undo',
+		actor: `${config.url}/users/${user._id}`,
+		object
+	};
+};
