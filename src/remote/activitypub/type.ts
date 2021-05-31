4b58f8546af5e168d100003d1a75f0481e4a0b48
mei23 <m@m544.net>
2021-05-31 16:00:56 +0900

    Fix IObject type

--- src/remote/activitypub/type.ts
+++ src/remote/activitypub/type.ts
@@ -6,7 +6,7 @@ export type ApObject = IObject | string | (IObject | string)[];
 
 export interface IObject {
 	'@context': string | obj | obj[];
-	type: string;
+	type: string | string[];
 	id?: string;
 	summary?: string;
 	published?: string;
