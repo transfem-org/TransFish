973b1e5dd464ea1ba43cac4082c3ffe11a03193f
mei23 <m@m544.net>
2021-11-27 22:58:58 +0900

    TwemojiをCDNに Resolve #3262

--- src/misc/twemoji-base.ts
+++ src/misc/twemoji-base.ts
@@ -1,1 +1,8 @@
-export const twemojiSvgBase = '/twemoji';
+// Official (Slow, no IPv6)
+// export const twemojiSvgBase = 'https://twemoji.maxcdn.com/v/latest/svg';
+
+// Alternative (Fast, with IPv6)
+export const twemojiSvgBase = 'https://cdn.jsdelivr.net/npm/@discordapp/twemoji@13.1.0/dist/svg';
+
+// Self hosting
+//export const twemojiSvgBase = '/twemoji';
