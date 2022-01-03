6506671b6316c9ddc907c45b3c760186cebbeee5
mei23 <m@m544.net>
2022-01-03 22:53:22 +0900

    Fix proxy

--- src/server/proxy/proxy-media.ts
+++ src/server/proxy/proxy-media.ts
@@ -18,14 +18,16 @@ export async function proxyMedia(ctx: Router.RouterContext) {
 
 		const { mime, ext } = await detectTypeWithCheck(path);
 
-		if (!(mime.startsWith('image/') && FILE_TYPE_BROWSERSAFE.includes(mime))) throw 403;
-
 		let image: IImage;
 
-		if ('static' in ctx.query && ['image/png', 'image/apng', 'image/gif', 'image/webp'].includes(mime)) {
+		if ('static' in ctx.query && ['image/png', 'image/apng', 'image/gif', 'image/webp', 'image/svg+xml'].includes(mime)) {
 			image = await convertToPng(path, 530, 255);
-		} else if ('preview' in ctx.query && ['image/jpeg', 'image/png', 'image/apng', 'image/gif', 'image/webp'].includes(mime)) {
+		} else if ('preview' in ctx.query && ['image/jpeg', 'image/png', 'image/apng', 'image/gif', 'image/webp', 'image/svg+xml'].includes(mime)) {
 			image = await convertToJpeg(path, 200, 200);
+		}	else if (['image/svg+xml'].includes(mime)) {
+			image = await convertToPng(path, 2048, 2048);
+		} else if (!mime.startsWith('image/') || !FILE_TYPE_BROWSERSAFE.includes(mime)) {
+			throw 403;
 		} else {
 			image = {
 				data: fs.readFileSync(path),
