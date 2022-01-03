bd2f233912dde647f291d8e2073560055c6085a3
mei23 <m@m544.net>
2022-01-03 22:24:42 +0900

    Fix remote svg emoji

--- src/services/emoji-store.ts
+++ src/services/emoji-store.ts
@@ -34,7 +34,7 @@ export async function tryStockEmoji(emoji: IEmoji) {
 
 export async function stockEmoji(emoji: IEmoji) {
 	const user = await getSystem1();
-	const file = await uploadFromUrl(emoji.url, user, null, emoji.url, false, true);
+	const file = await uploadFromUrl(emoji.url, user, null, null, false, true);
 
 	const url = getDriveFileUrl(file);
 
