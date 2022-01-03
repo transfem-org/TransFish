019c76e6c14851b42173b98f8b37dea91554c2e5
mei23 <m@m544.net>
2022-01-03 22:57:30 +0900

    Fix

--- src/server/proxy/proxy-media.ts
+++ src/server/proxy/proxy-media.ts
@@ -27,7 +27,7 @@ export async function proxyMedia(ctx: Router.RouterContext) {
 		}	else if (['image/svg+xml'].includes(mime)) {
 			image = await convertToPng(path, 2048, 2048);
 		} else if (!mime.startsWith('image/') || !FILE_TYPE_BROWSERSAFE.includes(mime)) {
-			throw 403;
+			ctx.throw(403);
 		} else {
 			image = {
 				data: fs.readFileSync(path),
