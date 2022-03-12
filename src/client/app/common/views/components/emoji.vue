6705c02ada4c1c2129445dc3ae8a1b3f361c1888
mei23 <m@m544.net>
2022-03-12 21:37:43 +0900

    Unicode 14.0 Emoji のassetsをとりあえずTwemoji版に

--- src/client/app/common/views/components/emoji.vue
+++ src/client/app/common/views/components/emoji.vue
@@ -350,9 +350,15 @@ export default Vue.extend({
 			// ローカル定義Twemoji
 			if (this.local || (this.isReaction && this.char.match(localEmojiRegex))) {
 				// Twemojiライクに、200d(joiner) を含まない場合は 最後の fe0f (絵文字セレクタを削除する)
-				if (!codes.includes('200d')) codes = codes.filter(x => x != 'fe0f');
+				//if (!codes.includes('200d')) codes = codes.filter(x => x != 'fe0f');
 				// で、参照先はTwemoji CDNじゃなくて...
-				this.url = `https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@main/svg/emoji_u${codes.join('-')}.svg`;
+				//this.url = `https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@main/svg/emoji_u${codes.join('-')}.svg`;
+
+				// Discord versionのnpmがpublishされるまで
+				let codes: string[] = Array.from(this.char).map(x => x.codePointAt(0).toString(16));
+				codes = codes.filter(x => x && x.length);
+				if (!codes.includes('200d')) codes = codes.filter(x => x != 'fe0f');
+				this.url = `https://twemoji.maxcdn.com/v/latest/svg/${codes.join('-')}.svg`;
 				return;
 			}
 
