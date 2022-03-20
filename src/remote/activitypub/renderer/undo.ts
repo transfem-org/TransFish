0af21a67362d990480bef8cc12c7dafc08816e36
mei23 <m@m544.net>
2022-03-20 19:03:10 +0900

    Revert "Unique id to Undo"
    This reverts commit f3f82ce2630ce401531684cfe6ebf78d0dbf1e32.

--- src/remote/activitypub/renderer/undo.ts
+++ src/remote/activitypub/renderer/undo.ts
@@ -5,7 +5,6 @@ export default (object: any, user: ILocalUser | IUser) => {
 	if (object == null) return null;
 
 	return {
-		id: (typeof object.id === 'string' && object.id.startsWith(config.url)) ? `${object.id}/undo` : undefined,
 		type: 'Undo',
 		actor: `${config.url}/users/${user._id}`,
 		object,
