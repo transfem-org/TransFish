3d77f4ea4f21d4af6c55b668084054e6e53fb425
mei23 <m@m544.net>
2022-03-21 16:24:43 +0900

    Unique id to AP Undo activity

--- src/remote/activitypub/renderer/undo.ts
+++ src/remote/activitypub/renderer/undo.ts
@@ -3,9 +3,11 @@ import { ILocalUser, IUser } from '../../../models/user';
 
 export default (object: any, user: ILocalUser | IUser) => {
 	if (object == null) return null;
+	const id = typeof object.id === 'string' && object.id.startsWith(config.url) ? `${object.id}/undo` : undefined;
 
 	return {
 		type: 'Undo',
+		...(id ? { id } : {}),
 		actor: `${config.url}/users/${user._id}`,
 		object,
 		published: new Date().toISOString(),
