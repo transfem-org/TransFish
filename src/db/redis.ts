b56387339c820456f6b9e64c0ce008e487f411dc
MeiMei <30769358+mei23@users.noreply.github.com>
2022-06-24 05:35:25 +0900

    Improve Redis (#3815)
    * ioredis
    
    * a

--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,12 +1,20 @@
-import * as redis from 'redis';
+import * as Redis from 'ioredis';
 import config from '../config';
 
-export default config.redis ? redis.createClient(
-	config.redis.port,
-	config.redis.host,
-	{
+function createConnection() {
+	return new Redis({
+		port: config.redis.port,
+		host: config.redis.host,
+		family: config.redis.family == null ? 0 : config.redis.family,
 		password: config.redis.pass,
-		prefix: config.redis.prefix,
-		db: config.redis.db || 0
-	}
-) : null;
+		keyPrefix: `${config.redis.prefix}:`,
+		db: config.redis.db || 0,
+	});
+}
+
+export const subsdcriber = createConnection();
+subsdcriber.subscribe(config.host);
+
+export const redisClient = createConnection();
+
+export default redisClient;
