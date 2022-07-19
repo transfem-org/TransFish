a36e097a1b5a5376650ceba108db1045c0c7049d
ThatOneCalculator <kainoa@t1c.dev>
2022-07-18 22:14:40 -0700

    self host

--- packages/client/src/pages/follow-requests.vue
+++ packages/client/src/pages/follow-requests.vue
@@ -5,7 +5,7 @@
 		<MkPagination ref="paginationComponent" :pagination="pagination">
 			<template #empty>
 				<div class="_fullinfo">
-					<img src="https://xn--931a.moe/assets/info.jpg" class="_ghost"/>
+					<img src="/static-assets/badges/info.jpg" class="_ghost"/>
 					<div>{{ $ts.noFollowRequests }}</div>
 				</div>
 			</template>
