0c46bb96b1b4e64adbce646b0f5af4a8e41ddc75
ThatOneCalculator <kainoa@t1c.dev>
2022-07-18 21:32:02 -0700

    The annoying orange

--- packages/backend/src/mfm/to-html.ts
+++ packages/backend/src/mfm/to-html.ts
@@ -141,7 +141,7 @@ export function toHtml(nodes: mfm.MfmNode[] | null, mentionedRemoteUsers: IMenti
 
 		search(node) {
 			const a = doc.createElement('a');
-			a.href = `https://www.google.com/search?q=${node.props.query}`;
+			a.href = `https://search.annoyingorange.xyz/search?q=${node.props.query}`;
 			a.textContent = node.props.content;
 			return a;
 		},
