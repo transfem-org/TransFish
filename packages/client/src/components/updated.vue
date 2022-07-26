b036c9d490a8403ff9e99bad5026a86795496aa0
ThatOneCalculator <kainoa@t1c.dev>
2022-07-26 14:31:46 -0700

    Remove updated.vue for now

--- packages/client/src/components/updated.vue
+++ packages/client/src/components/updated.vue
@@ -21,7 +21,7 @@ const modal = ref<InstanceType<typeof MkModal>>();
 
 const whatIsNew = () => {
 	modal.value.close();
-	window.open(`https://misskey-hub.net/docs/releases.html#_${version.replace(/\./g, '-')}`, '_blank');
+	window.open('https://codeberg.org/thatonecalculator/calckey/releases', '_blank');
 };
 </script>
 
