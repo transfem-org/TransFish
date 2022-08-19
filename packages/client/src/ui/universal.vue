4932ab2c349cd827144a097c17c9290cfed7fceb
ThatOneCalculator <kainoa@t1c.dev>
2022-08-18 17:37:33 -0700

    fix: :bug: fix again

--- packages/client/src/ui/universal.vue
+++ packages/client/src/ui/universal.vue
@@ -25,7 +25,7 @@
 		<button class="button widget _button" @click="widgetsShowing = true"><i class="fas fa-layer-group"></i></button>
 	</div>
 
-	<button v-if="isMobile" class="postButton button post _button" @click="os.post()"><i class="fas fa-pencil-alt"></i></button>
+	<button v-if="isMobile" id="postButton" class="postButton button post _button" @click="os.post()"><i class="fas fa-pencil-alt"></i></button>
 
 	<transition :name="$store.state.animation ? 'menuDrawer-back' : ''">
 		<div
@@ -172,7 +172,7 @@ function top() {
 }
 
 let scrollPos = 0;
-const postButton = document.querySelector<HTMLElement>('.postButton');
+const postButton = document.getElementById('postButton');
 
 function checkPosition() {
 	let windowY = window.scrollY;
