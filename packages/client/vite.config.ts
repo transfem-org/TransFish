2daa02c642c7e37182b70ec94d1aafeb64b1099a
lunaisnotaboy <her@mint.lgbt>
2022-10-26 07:24:37 -0400

    Remove SWC (thank you @lunanova)

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,7 +1,6 @@
 import * as fs from 'fs';
 import pluginVue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
-import swc from 'unplugin-swc';
 
 import locales from '../../locales';
 import meta from '../../package.json';
@@ -21,7 +20,6 @@ export default defineConfig(({ command, mode }) => {
 				reactivityTransform: true,
 			}),
 			pluginJson5(),
-			swc.vite(),
 		],
 
 		resolve: {
