ec237623f2cad816b8b7f392f0f0601da076955b
ThatOneCalculator <kainoa@t1c.dev>
2022-11-18 20:18:00 -0800

    typo

--- packages/backend/migration/1668831378728FixCalckeyAgain.js
+++ packages/backend/migration/1668831378728FixCalckeyAgain.js
@@ -2,7 +2,7 @@ export class FixCalckeyAgain1668831378728 {
 		name = 'FixCalckeyAgain1668831378728'
 
 	async up(queryRunner) {
-		await queryRunner.query(`UPDATE "meta" SET "useStarForReactionFallback" = TRUE`);=
+		await queryRunner.query(`UPDATE "meta" SET "useStarForReactionFallback" = TRUE`);
 	}
 
 	async down(queryRunner) {
