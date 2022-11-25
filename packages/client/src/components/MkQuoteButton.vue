4b3006bead89a3446b47d4178f21aa192c5202e5
yawhn <kordaris@gmail.com>
2022-11-25 13:46:10 +0200

    hover text in note buttons

--- packages/client/src/components/MkQuoteButton.vue
+++ packages/client/src/components/MkQuoteButton.vue
@@ -2,6 +2,7 @@
 <button
 	v-if="canRenote && $store.state.seperateRenoteQuote"
 	class="eddddedb _button"
+	v-tooltip.noDelay.bottom="i18n.ts.quote"
 	@click="quote()"
 >
 	<i class="ph-quotes-bold ph-lg"></i>
@@ -14,6 +15,7 @@ import type { Note } from 'misskey-js/built/entities';
 import { pleaseLogin } from '@/scripts/please-login';
 import * as os from '@/os';
 import { $i } from '@/account';
+import { i18n } from '@/i18n';
 
 const props = defineProps<{
 	note: Note;
