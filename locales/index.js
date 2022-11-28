31af36e57eb48b60f108ca5e550c153aa9ec35ba
CherryKitten <git@cherrykitten.dev>
2022-11-28 23:43:24 +0100

    Don't hardcode locales

--- locales/index.js
+++ locales/index.js
@@ -13,33 +13,14 @@ const merge = (...args) => args.reduce((a, c) => ({
 		.reduce((a, [k, v]) => (a[k] = merge(v, c[k]), a), {})
 }), {});
 
-const languages = [
-	'ar-SA',
-	'cs-CZ',
-	'da-DK',
-	'de-DE',
-	'en-US',
-	'es-ES',
-	'fr-FR',
-	'id-ID',
-	'it-IT',
-	'ja-JP',
-	'ja-KS',
-	'kab-KAB',
-	'kn-IN',
-	'ko-KR',
-	'nl-NL',
-	'no-NO',
-	'pl-PL',
-	'pt-PT',
-	'ru-RU',
-	'sk-SK',
-	'ug-CN',
-	'uk-UA',
-	'vi-VN',
-	'zh-CN',
-	'zh-TW',
-];
+languages = []
+
+fs.readdirSync(__dirname).forEach((file) => {
+	if (file.includes('.yml')){
+		file = file.slice(0, file.indexOf('.'))
+		languages.push(file)
+	}
+})
 
 const primaries = {
 	'en': 'US',
