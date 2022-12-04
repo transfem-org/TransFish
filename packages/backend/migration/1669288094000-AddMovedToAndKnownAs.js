a4916d8024c89cb67c5c1847dc72c72987a50a04
ThatOneCalculator <kainoa@t1c.dev>
2022-12-04 13:26:24 -0800

    fix migration

--- packages/backend/migration/1669288094000-AddMovedToAndKnownAs.js
+++ packages/backend/migration/1669288094000-AddMovedToAndKnownAs.js
@@ -3,14 +3,14 @@ export class addMovedToAndKnownAs1669288094000 {
 
 	async up(queryRunner) {
 			await queryRunner.query(`ALTER TABLE "user" ADD "movedToUri" character varying(512)`);
-			await queryRunner.query(`ALTER TABLE "user" ADD "knownAs" TEXT []`);
+			await queryRunner.query(`ALTER TABLE "user" ADD "alsoKnownAs" TEXT []`);
 			await queryRunner.query(`COMMENT ON COLUMN "user"."movedToUri" IS 'The URI of the new account of the User'`);
-			await queryRunner.query(`COMMENT ON COLUMN "user"."knownAs" IS 'URIs the user is known as too'`);
+			await queryRunner.query(`COMMENT ON COLUMN "user"."alsoKnownAs" IS 'URIs the user is known as too'`);
 	}
 
 	async down(queryRunner) {
 			await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "movedToUri"`);
-			await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "knownAs"`);
+			await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "alsoKnownAs"`);
 	}
 
 }
