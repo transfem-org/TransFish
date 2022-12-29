ba4680c5127206c54f840883f056696d48b055c9
Kaity A <supakaity@blahaj.zone>
2022-12-29 10:00:30 +0000

    add antenna mark read functionalityu

--- packages/client/src/pages/my-antennas/index.vue
+++ packages/client/src/pages/my-antennas/index.vue
@@ -7,9 +7,14 @@
 
 			<div class="">
 				<MkPagination v-slot="{items}" ref="list" :pagination="pagination">
-					<MkA v-for="antenna in items" :key="antenna.id" class="ljoevbzj" :to="`/my/antennas/${antenna.id}`">
-						<div class="name">{{ antenna.name }}</div>
-					</MkA>
+					<div v-for="antenna in items" :key="antenna.id">
+						<MkA class="uopelskx" :link="true" :to="`/timeline/antenna/${antenna.id}`">
+							<i class="ph-flying-saucer-bold ph-lg"></i><i :class="`${antenna.hasUnreadNote ? 'ph-circle-fill' : 'ph-check'} ph-xs notify-icon`"></i>
+						</MkA>
+						<MkA class="ljoevbzj" :to="`/my/antennas/${antenna.id}`">
+							<div class="name">{{ antenna.name }}</div>
+						</MkA>
+					</div>
 				</MkPagination>
 			</div>
 		</div>
@@ -18,7 +23,7 @@
 </template>
 
 <script lang="ts" setup>
-import { } from 'vue';
+import { onActivated, onDeactivated, ref } from 'vue';
 import MkPagination from '@/components/MkPagination.vue';
 import MkButton from '@/components/MkButton.vue';
 import { i18n } from '@/i18n';
@@ -26,12 +31,37 @@ import { definePageMetadata } from '@/scripts/page-metadata';
 
 const pagination = {
 	endpoint: 'antennas/list' as const,
-	limit: 10,
+	limit: 250,
 };
 
 const headerActions = $computed(() => []);
 
 const headerTabs = $computed(() => []);
+
+const list = ref<typeof MkPagination|null>(null);
+
+let isCached: boolean = false;
+let refreshTimer: number | null = null;
+
+const refresh = () => {
+	if (isCached) {
+		list.value?.refresh();
+	}
+
+	isCached = true;
+	refreshTimer = setTimeout(refresh, 15000)
+};
+
+onActivated(() => {
+	refresh();
+});
+
+onDeactivated(() => {
+	if (refreshTimer) {
+		clearTimeout(refreshTimer);
+		refreshTimer = null;
+	}
+});
 
 definePageMetadata({
 	title: i18n.ts.manageAntennas,
@@ -44,6 +74,20 @@ definePageMetadata({
 
 	> .add {
 		margin: 0 auto 16px auto;
+	}
+
+	.uopelskx {
+		float: left;
+		min-width: 25px;
+		padding: 13px;
+		margin-right: 10px;
+		border: solid 1px var(--divider);
+		border-radius: 6px;
+
+		&:hover {
+			border: solid 1px var(--accent);
+			text-decoration: none;
+		}
 	}
 
 	.ljoevbzj {
@@ -60,6 +104,21 @@ definePageMetadata({
 
 		> .name {
 			font-weight: bold;
+		}
+	}
+
+	.notify-icon {
+    position: relative;
+    top: -1em;
+    left: -0.5em;
+
+		&.ph-circle-fill {
+			color: var(--indicator);
+			animation: blink 1s infinite;
+		}
+
+		&.ph-check {
+			color: var(--muted);
 		}
 	}
 }
