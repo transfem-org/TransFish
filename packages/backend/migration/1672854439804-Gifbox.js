85140d8a97bef5ccc5f787796b2b00ab15bce15a
ThatOneCalculator <kainoa@t1c.dev>
2023-01-08 11:34:37 -0800

    gifbox migration

--- packages/backend/migration/1672854439804-Gifbox.js
+++ packages/backend/migration/1672854439804-Gifbox.js
@@ -3,9 +3,9 @@ export class gifboxIntegration1672854439804 {
         this.name = 'gifboxIntegration1672854439804';
     }
     async up(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "meta" ADD "deeplAuthKey" character varying(128)`);
+        await queryRunner.query(`ALTER TABLE "meta" ADD "gifboxAuthKey" character varying(128)`);
     }
     async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "deeplAuthKey"`);
+        await queryRunner.query(`ALTER TABLE "meta" DROP COLUMN "gifboxAuthKey"`);
     }
 }
