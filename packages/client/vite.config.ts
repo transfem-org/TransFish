83fe5ca70170c96b306bfb971b100b11076176d2
ThatOneCalculator <kainoa@t1c.dev>
2023-01-10 08:49:15 -0800

    refactor: :wrench: vite config
    Co-authored-by: Syuilo <Syuilotan@yahoo.co.jp>

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,17 +1,43 @@
-import * as fs from 'fs';
 import pluginVue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
+import webfontDownload from 'vite-plugin-webfont-dl';
 import locales from '../../locales';
 import meta from '../../package.json';
 import pluginJson5 from './vite.json5';
 
 const extensions = ['.ts', '.tsx', '.js', '.jsx', '.mjs', '.json', '.json5', '.svg', '.sass', '.scss', '.css', '.vue'];
 
-export default defineConfig(({ command, mode }) => {
-	fs.mkdirSync(__dirname + '/../../built', { recursive: true });
-	fs.writeFileSync(__dirname + '/../../built/meta.json', JSON.stringify({ version: meta.version }), 'utf-8');
+const hash = (str: string, seed = 0): number => {
+	let h1 = 0xdeadbeef ^ seed,
+		h2 = 0x41c6ce57 ^ seed;
+	for (let i = 0, ch; i < str.length; i++) {
+		ch = str.charCodeAt(i);
+		h1 = Math.imul(h1 ^ ch, 2654435761);
+		h2 = Math.imul(h2 ^ ch, 1597334677);
+	}
+	
+	h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+	h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+	
+	return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
 
+const BASE62_DIGITS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+function toBase62(n: number): string {
+	if (n === 0) {
+		return '0';
+	}
+	let result = ''; 
+	while (n > 0) {
+		result = BASE62_DIGITS[n % BASE62_DIGITS.length] + result;
+		n = Math.floor(n / BASE62_DIGITS.length);
+	}
+	
+	return result;
+}
+
+export default defineConfig(({ command, mode }) => {
 	return {
 		base: '/assets/',
 
@@ -20,6 +46,7 @@ export default defineConfig(({ command, mode }) => {
 				reactivityTransform: true,
 			}),
 			pluginJson5(),
+			webfontDownload(),
 		],
 
 		resolve: {
@@ -28,6 +55,14 @@ export default defineConfig(({ command, mode }) => {
 				'@/': __dirname + '/src/',
 				'/client-assets/': __dirname + '/assets/',
 				'/static-assets/': __dirname + '/../backend/assets/',
+			},
+		},
+
+		css: {
+			modules: {
+				generateScopedName: (name, filename, css) => {
+					return 'x' + toBase62(hash(`${filename} ${name}`)).substring(0, 4);
+				},
 			},
 		},
 
@@ -46,10 +81,9 @@ export default defineConfig(({ command, mode }) => {
 
 		build: {
 			target: [
-				'chrome100',
-				'firefox100',
-				'safari15',
-				'es2021', // TODO: keep this up to date
+				'chrome108',
+				'firefox109',
+				'safari16',
 			],
 			manifest: 'manifest.json',
 			rollupOptions: {
@@ -63,15 +97,14 @@ export default defineConfig(({ command, mode }) => {
 				},
 			},
 			cssCodeSplit: true,
-			assetsInlineLimit: 0,
 			outDir: __dirname + '/../../built/_client_dist_',
 			assetsDir: '.',
 			emptyOutDir: false,
-			sourcemap: process.env.NODE_ENV !== 'production',
-			reportCompressedSize: false,
+			sourcemap: process.env.NODE_ENV === 'development',
+			reportCompressedSize: true,
 		},
 		optimizeDeps: {
-			auto: true
+			auto: true,
 		},
 	};
 });
