df331269bf969a360b763d2e0112051f0fd29687
ThatOneCalculator <kainoa@t1c.dev>
2023-01-16 11:46:09 -0800

    fix: avatars in grid, not seperate grids

--- packages/client/src/components/MkAvatars.vue
+++ packages/client/src/components/MkAvatars.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="defgtij">
-	<div v-for="user in users" :key="user.id" class="users">
-		<MkAvatar :user="user" class="avatar" :show-indicator="true"/>
+	<div v-for="user in users" :key="user.id" class="avatar">
+		<MkAvatar :user="user" :show-indicator="true"/>
 	</div>
 </div>
 </template>
@@ -26,19 +26,15 @@ onMounted(async () => {
 <style lang="scss">
 .defgtij {
 	padding: 12px;
+	grid-gap: 12px;
+	display: grid;
+	grid-template-columns: repeat(auto-fill, minmax(30px, 40px));
+	place-content: center;
 
-	> .users {
-		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(30px, 40px));
-		grid-gap: 12px;
-		place-content: center;
-		padding: 12px;
-
-		> .avatar {
-			width: 100%;
-			height: 100%;
-			aspect-ratio: 1;
-		}
+	> .avatar {
+		width: 100%;
+		height: 100%;
+		aspect-ratio: 1;
 	}
 }
 </style>
