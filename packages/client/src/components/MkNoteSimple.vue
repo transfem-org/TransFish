0ff2f2172e38cf87fc273844c52e2d036529831e
Freeplay <Freeplay@duck.com>
2023-01-21 14:39:19 -0500

    add back pfp in compose box when replying

--- packages/client/src/components/MkNoteSimple.vue
+++ packages/client/src/components/MkNoteSimple.vue
@@ -1,6 +1,6 @@
 <template>
 <div v-size="{ min: [350, 500] }" class="yohlumlk">
-	<!-- <MkAvatar class="avatar" :user="note.user"/> -->
+	<MkAvatar class="avatar" :user="note.user"/>
 	<div class="main">
 		<XNoteHeader class="header" :note="note" :mini="true"/>
 		<div class="body">
