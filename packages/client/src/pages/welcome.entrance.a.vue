7df330ec296b7c57a4b2a38317e2c00887406c8f
ThatOneCalculator <kainoa@t1c.dev>
2023-01-23 12:12:49 -0800

    fix: :bug: double name on splash
    Closes #9500

--- packages/client/src/pages/welcome.entrance.a.vue
+++ packages/client/src/pages/welcome.entrance.a.vue
@@ -24,8 +24,8 @@
 			<button class="_button _acrylic menu" @click="showMenu"><i class="ph-dots-three-outline-bold ph-lg"></i></button>
 			<div class="fg">
 				<h1>
-					<img class="logo" v-if="meta.logoImageUrl" :src="meta.logoImageUrl"><span v-else class="text">{{ instanceName }}</span>
-					<span class="text">{{ instanceName }}</span>
+					<img class="logo" v-if="meta.logoImageUrl" :src="meta.logoImageUrl">
+					<span v-else class="text">{{ instanceName }}</span>
 				</h1>
 				<div class="about">
 					<div class="desc" v-html="meta.description || i18n.ts.headlineMisskey"></div>
