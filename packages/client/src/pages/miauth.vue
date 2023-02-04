96e1a93216eab6e42da37411becc9d997aaddb05
syuilo <Syuilotan@yahoo.co.jp>
2023-02-04 09:10:01 +0900

    fix(client): validate urls to improve security

--- packages/client/src/pages/miauth.vue
+++ packages/client/src/pages/miauth.vue
@@ -70,6 +70,8 @@ async function accept(): Promise<void> {
 
 	state = 'accepted';
 	if (props.callback) {
+		const cbUrl = new URL(props.callback);
+		if (!['http:', 'https:'].includes(cbUrl.protocol)) throw new Error('invalid url');
 		location.href = appendQuery(props.callback, query({
 			session: props.session,
 		}));
