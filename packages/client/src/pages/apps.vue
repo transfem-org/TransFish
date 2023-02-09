c7be56e1c98e27c0cb270838dabaa724bf0ab381
ThatOneCalculator <kainoa@t1c.dev>
2023-02-08 18:29:44 -0800

    help buttons

--- packages/client/src/pages/apps.vue
+++ packages/client/src/pages/apps.vue
@@ -4,17 +4,35 @@
 	<div style="overflow: clip;">
 		<MkSpacer :content-max="600" :margin-min="20">
 			<div class="_formRoot">
+				<div class="_formBlock" style="text-align: center;">
+					<pwa-install/>
+					<MkButton primary rounded inline @click="installPwa">{{ i18n.ts._apps.pwa }}</MkButton>
+				</div>
 				<FormSection>
 					<template #label>{{ i18n.ts._apps.crossPlatform }}</template>
 					<div class="_formLinks">
-						<div class="_formBlock" style="text-align: center;">
-							<pwa-install/>
-							<MkButton primary rounded inline @click="installPwa">{{ i18n.ts._apps.pwa }}</MkButton>
-						</div>
 						<FormLink to="https://kaiteki.app" external>
 							<template #icon>
 								<i class="ph-device-mobile-bold ph-xl"/>
 								<i class="ph-android-logo-bold ph-lg"/>
+								<span> </span>
+								<i class="ph-desktop-bold ph-xl"/>
+								<i class="ph-windows-logo-bold ph-lg"/>
+								<i class="ph-linux-logo-bold ph-lg"/>
+							</template>
+							{{ i18n.ts._apps.kaiteki }}
+							<template #suffix>{{ i18n.ts._apps.firstClass }}</template>
+						</FormLink>
+					</div>
+				</FormSection>
+				<FormSection>
+					<template #label>{{ i18n.ts._apps.crossPlatform }}</template>
+					<div class="_formLinks">
+						<FormLink to="https://kaiteki.app" external>
+							<template #icon>
+								<i class="ph-device-mobile-bold ph-xl"/>
+								<i class="ph-android-logo-bold ph-lg"/>
+								<span> </span>
 								<i class="ph-desktop-bold ph-xl"/>
 								<i class="ph-windows-logo-bold ph-lg"/>
 								<i class="ph-linux-logo-bold ph-lg"/>
