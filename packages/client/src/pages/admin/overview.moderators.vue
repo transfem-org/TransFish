694ce70d6e52c067cb69b24ae8b8b284fd50d70c
ThatOneCalculator <kainoa@t1c.dev>
2023-02-15 12:45:41 -0800

    Revert "refactor: use MkAvatars for mods"
    This reverts commit dbe74ed098d06efb2e425d5d93d45798abdf350c.

--- packages/client/src/pages/admin/overview.moderators.vue
+++ packages/client/src/pages/admin/overview.moderators.vue
@@ -3,7 +3,9 @@
 	<Transition :name="$store.state.animation ? '_transition_zoom' : ''" mode="out-in">
 		<MkLoading v-if="fetching"/>
 		<div v-else :class="$style.root" class="_panel">
-			<MkAvatars :user-ids="moderators.userIds" class="userAvatars"/>
+			<MkA v-for="user in moderators" :key="user.id" class="user" :to="`/user-info/${user.id}`">
+				<MkAvatar :user="user" class="avatar" indicator/>
+			</MkA>
 		</div>
 	</Transition>
 </div>
@@ -12,7 +14,8 @@
 <script lang="ts" setup>
 import { onMounted, onUnmounted, ref } from 'vue';
 import * as os from '@/os';
-import MkAvatars from '@/components/MkAvatars.vue';
+import number from '@/filters/number';
+import { i18n } from '@/i18n';
 
 let moderators: any = $ref(null);
 let fetching = $ref(true);
@@ -35,5 +38,18 @@ onMounted(async () => {
 	grid-gap: 12px;
 	place-content: center;
 	padding: 12px;
+
+	&:global {
+		> .user {
+			width: 100%;
+			height: 100%;
+			aspect-ratio: 1;
+
+			> .avatar {
+				width: 100%;
+				height: 100%;
+			}
+		}
+	}
 }
 </style>
