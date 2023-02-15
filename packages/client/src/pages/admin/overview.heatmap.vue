020101a2fc006bca01fac893f1df8d11f272d68c
ThatOneCalculator <kainoa@t1c.dev>
2023-02-15 10:45:33 -0800

    notes for admin heatmap by default

--- packages/client/src/pages/admin/overview.heatmap.vue
+++ packages/client/src/pages/admin/overview.heatmap.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="_panel" :class="$style.root">
 	<MkSelect v-model="src" style="margin: 0 0 12px 0;" small>
-		<option value="active-users">Active users</option>
 		<option value="notes">Notes</option>
+		<option value="active-users">Active users</option>
 		<option value="ap-requests-inbox-received">Fediverse Requests: inboxReceived</option>
 		<option value="ap-requests-deliver-succeeded">Fediverse Requests: deliverSucceeded</option>
 		<option value="ap-requests-deliver-failed">Fediverse Requests: deliverFailed</option>
@@ -15,7 +15,7 @@
 import MkHeatmap from '@/components/MkHeatmap.vue';
 import MkSelect from '@/components/form/select.vue';
 
-let src = $ref('active-users');
+let src = $ref('notes');
 </script>
 
 <style lang="scss" module>
