2422a8fe7a0e5272e8cd8824cc60334b547a9687
ThatOneCalculator <kainoa@t1c.dev>
2023-03-16 18:42:45 -0700

    🤦

--- packages/backend/src/services/note/polls/vote.ts
+++ packages/backend/src/services/note/polls/vote.ts
@@ -4,7 +4,6 @@ import type { CacheableUser } from "@/models/entities/user.js";
 import { User } from "@/models/entities/user.js";
 import type { Note } from "@/models/entities/note.js";
 import { PollVotes, NoteWatchings, Polls, Blockings } from "@/models/index.js";
-import { Not } from "typeorm";
 import { genId } from "@/misc/gen-id.js";
 import { createNotification } from "../../create-notification.js";
 
