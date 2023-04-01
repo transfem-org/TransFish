93867ec927c0fa06b5aa1c617f73268153874ed8
ThatOneCalculator <kainoa@t1c.dev>
2023-04-01 12:14:54 -0700

    fix migration

--- packages/backend/migration/1680375641101-clean-charts.js
+++ packages/backend/migration/1680375641101-clean-charts.js
@@ -1,15 +1,17 @@
 export class CleanCharts1680375641101 {
-    constructor() {
-        this.name = 'CleanCharts1680375641101';
-    }
-    async up(queryRunner) {
-        await queryRunner.query(`delete from __chart__hashtag where ___local_users = 0 and ___remote_users = 0;`);
+		constructor() {
+				this.name = 'CleanCharts1680375641101';
+		}
+		async up(queryRunner) {
+				await queryRunner.query(`delete from __chart__hashtag where ___local_users = 0 and ___remote_users = 0;`);
 				await queryRunner.query(`delete from __chart_day__hashtag where ___local_users = 0 and ___remote_users = 0;`);
+				await queryRunner.query(`COMMIT;`);
 				await queryRunner.query(`vacuum __chart__hashtag;`);
 				await queryRunner.query(`vacuum __chart_day__hashtag;`);
-    }
-    async down(queryRunner) {
-			await queryRunner.query(`vacuum __chart__hashtag;`);
-			await queryRunner.query(`vacuum __chart_day__hashtag;`);
-    }
+				await queryRunner.query(`COMMIT;`);
+		}
+		async down(queryRunner) {
+			await queryRunner.query(`delete from __chart__hashtag where ___local_users = 0 and ___remote_users = 0;`);
+			await queryRunner.query(`delete from __chart_day__hashtag where ___local_users = 0 and ___remote_users = 0;`);
+		}
 }
