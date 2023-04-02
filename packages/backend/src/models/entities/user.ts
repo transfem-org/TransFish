99a21e70de659dec7aa6a52885cb2bf5e4c53dc6
Kaity A <kaity@theallans.com.au>
2023-04-02 21:24:12 +1000

    Separate cat-avatar mode and speak-as-cat nyanification

--- packages/backend/src/models/entities/user.ts
+++ packages/backend/src/models/entities/user.ts
@@ -157,6 +157,12 @@ export class User {
 	public isCat: boolean;
 
 	@Column('boolean', {
+		default: true,
+		comment: 'Whether to speak as a cat if isCat.',
+	})
+	public speakAsCat: boolean;
+
+	@Column('boolean', {
 		default: false,
 		comment: 'Whether the User is the admin.',
 	})
