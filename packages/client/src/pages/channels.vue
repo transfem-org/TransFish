54f6876c9ca3265a39d14e0770fbfd0e5f40d05d
たーびん <tar.bin.master@gmail.com>
2023-04-11 07:42:27 +0900

    feat: チャンネルの検索用ページとAPIの追加
    * add channel search
    
    * move  channel search to channel list page
    
    ---------
    
    Co-authored-by: tamaina <tamaina@hotmail.co.jp>
    Co-authored-by: syuilo <Syuilotan@yahoo.co.jp>
    Co-authored-by: atsuchan <83960488+atsu1125@users.noreply.github.com>
    Co-authored-by: Masaya Suzuki <15100604+massongit@users.noreply.github.com>
    Co-authored-by: Kagami Sascha Rosylight <saschanaz@outlook.com>
    Co-authored-by: taiy <53635909+taiyme@users.noreply.github.com>
    Co-authored-by: xianon <xianon@hotmail.co.jp>
    Co-authored-by: kabo2468 <28654659+kabo2468@users.noreply.github.com>
    Co-authored-by: YS <47836716+yszkst@users.noreply.github.com>
    Co-authored-by: Khsmty <me@khsmty.com>
    Co-authored-by: Soni L <EnderMoneyMod@gmail.com>
    Co-authored-by: mei23 <m@m544.net>
    Co-authored-by: daima3629 <52790780+daima3629@users.noreply.github.com>
    Co-authored-by: Windymelt <1113940+windymelt@users.noreply.github.com>

--- packages/client/src/pages/channels.vue
+++ packages/client/src/pages/channels.vue
@@ -21,6 +21,24 @@
 				@slide-change="onSlideChange"
 			>
 				<swiper-slide>
+					<div class="_content grwlizim search">
+						<div class="gaps">
+							<MkInput v-model="searchQuery" :large="true" :autofocus="true" type="search">
+								<template #prefix><i class="ti ti-search"></i></template>
+							</MkInput>
+							<MkRadios v-model="searchType" @update:model-value="search()">
+								<option value="nameAndDescription">{{ i18n.ts._channel.nameAndDescription }}</option>
+								<option value="nameOnly">{{ i18n.ts._channel.nameOnly }}</option>
+							</MkRadios>
+							<MkButton large primary gradate rounded @click="search">{{ i18n.ts.search }}</MkButton>
+						</div>
+						<MkFoldableSection v-if="channelPagination">
+							<template #header>{{ i18n.ts.searchResult }}</template>
+							<MkChannelList :key="key" :pagination="channelPagination"/>
+						</MkFoldableSection>
+					</div>
+				</swiper-slide>
+				<swiper-slide>
 					<div class="_content grwlizim featured">
 						<MkPagination
 							v-slot="{ items }"
@@ -74,12 +92,16 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, defineComponent, inject, watch } from "vue";
+import { computed, onMounted, defineComponent, inject, watch } from "vue";
 import { Virtual } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import MkChannelPreview from "@/components/MkChannelPreview.vue";
+import MkChannelList from '@/components/MkChannelList.vue';
 import MkPagination from "@/components/MkPagination.vue";
+import MkInput from '@/components/MkInput.vue';
+import MkRadios from '@/components/MkRadios.vue';
 import MkButton from "@/components/MkButton.vue";
+import MkFoldableSection from '@/components/MkFoldableSection.vue';
 import { useRouter } from "@/router";
 import { definePageMetadata } from "@/scripts/page-metadata";
 import { deviceKind } from "@/scripts/device-kind";
@@ -90,9 +112,23 @@ import "swiper/scss/virtual";
 
 const router = useRouter();
 
-const tabs = ["featured", "following", "owned"];
-let tab = $ref(tabs[0]);
+const tabs = ["search", "featured", "following", "owned"];
+let tab = $ref(tabs[1]);
 watch($$(tab), () => syncSlide(tabs.indexOf(tab)));
+
+const props = defineProps<{
+	query: string;
+	type?: string;
+}>();
+let key = $ref('');
+let tab = $ref('search');
+let searchQuery = $ref('');
+let searchType = $ref('nameAndDescription');
+let channelPagination = $ref();
+onMounted(() => {
+	searchQuery = props.query ?? '';
+	searchType = props.type ?? 'nameAndDescription';
+});
 
 const featuredPagination = {
 	endpoint: "channels/featured" as const,
@@ -108,6 +144,21 @@ const ownedPagination = {
 	limit: 10,
 };
 
+async function search() {
+	const query = searchQuery.toString().trim();
+	if (query == null || query === '') return;
+	const type = searchType.toString().trim();
+	channelPagination = {
+		endpoint: 'channels/search',
+		limit: 10,
+		params: {
+			query: searchQuery,
+			type: type,
+		},
+	};
+	key = query + type;
+}
+
 function create() {
 	router.push("/channels/new");
 }
@@ -121,6 +172,11 @@ const headerActions = $computed(() => [
 ]);
 
 const headerTabs = $computed(() => [
+	{
+		key: 'search',
+		title: i18n.ts.search,
+		icon: 'ti ti-search',
+	},
 	{
 		key: "featured",
 		title: i18n.ts._channel.featured,
