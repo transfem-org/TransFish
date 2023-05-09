d86a5a714774a15f3e561f39f934db6bc33cd026
s1idewhist1e <trombonedude05@gmail.com>
2023-05-09 00:13:21 -0700

    Add config iniitialization (I can totally spell)

--- packages/backend/src/main.rs
+++ packages/backend/src/main.rs
@@ -1,17 +1,30 @@
+use std::{
+    env, error, fmt,
+    path::{Path, PathBuf},
+};
 
-use std::{path::Path, error::Error};
-
-#[cfg(debug_assertions)]
 extern crate config;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn error::Error>> {
+    env::set_var(
+        "CK_REPO_DIR",
+        PathBuf::from(env!("PWD"))
+            .parent()
+            .and_then(|p| p.parent())
+            .ok_or(fmt::Error)?,
+    );
     // bootstrap
 
     // ENV
 
     // get config
 
-    config::init_config(Path::new(""))?;
+    config::init_config(
+        &Path::new(&env::var("CK_REPO_DIR")?)
+            .join(".config")
+            .join("default.yml"),
+    )?;
 
+    println!("{:?}", config::get_config()?);
     Ok(())
 }
