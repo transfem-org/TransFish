e1a0ab7d25bd1b48f7e02297d5efd22cf7aef463
Namekuji <nmkj@mx.kazuno.co>
2023-05-13 05:22:28 -0400

    use thiserror for impl Error and Display

--- packages/backend/src/main.rs
+++ packages/backend/src/main.rs
@@ -1,12 +1,14 @@
 use std::{
-    env, error, fmt,
+    env, fmt,
     path::{Path, PathBuf},
 };
+use tracing::debug;
 
 #[macro_use]
 extern crate macros;
 
-fn main() -> Result<(), Box<dyn error::Error>> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     env::set_var(
         "CK_REPO_DIR",
         PathBuf::from(env!("PWD"))
@@ -14,17 +16,33 @@ fn main() -> Result<(), Box<dyn error::Error>> {
             .and_then(|p| p.parent())
             .ok_or(fmt::Error)?,
     );
+
+    // logging
+    let is_debug = match env::var_os("NODE_ENV") {
+        None => true,
+        Some(val) => val != "production",
+    };
+    let subscriber = tracing_subscriber::fmt();
+    if is_debug {
+        subscriber
+            .with_max_level(tracing::Level::DEBUG)
+            .pretty()
+            .init();
+    } else {
+        subscriber.with_max_level(tracing::Level::INFO).init();
+    }
+
     // bootstrap
 
     // ENV
 
     // get config
 
-    config::init_config(
-        &Path::new(&env::var("CK_REPO_DIR")?)
-            .join(".config")
-            .join("default.yml"),
-    )?;
+    let config_path = &Path::new(&env::var("CK_REPO_DIR")?)
+        .join(".config")
+        .join("default.yml");
+    debug!(target: "config", path = ?config_path, "Loading yaml file");
+    config::init_config(config_path)?;
 
     eprintln!("{:?}", config::get_config()?);
 
