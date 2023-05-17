da522482aca45497060d2af6e40225dc7484a423
Freeplay <Freeplay@duck.com>
2023-05-17 13:46:00 -0400

    Allow pressing esc to close lightbox

--- packages/client/src/components/MkMediaList.vue
+++ packages/client/src/components/MkMediaList.vue
@@ -166,6 +166,8 @@ onMounted(() => {
 	lightbox.on("afterInit", () => {
 		history.pushState(null, "", location.href);
 		addEventListener("popstate", close);
+		// This is a workaround. Not sure why, but when clicking to open, it doesn't move focus to the photoswipe. Preventing using esc to close. However when using keyboard to open it already focuses the lightbox fine.
+		lightbox.pswp.element.focus();
 	})
 	lightbox.on("close", () => {
 		removeEventListener("popstate", close);
