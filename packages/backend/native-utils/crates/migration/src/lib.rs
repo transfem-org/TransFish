d9d2673939e1cae99fe6df9b061d50bbacb68c60
Namekuji <nmkj@wahh.foo>
2023-06-01 12:32:15 -0400

    use thread to copy data

--- packages/backend/native-utils/crates/migration/src/lib.rs
+++ packages/backend/native-utils/crates/migration/src/lib.rs
@@ -1,12 +1,12 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20230531_180824_stringvec;
+mod m20230531_180824_drop_reversi;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20230531_180824_stringvec::Migration)]
+        vec![Box::new(m20230531_180824_drop_reversi::Migration)]
     }
 }
--- packages/backend/native-utils/crates/migration/src/main.rs
+++ packages/backend/native-utils/crates/migration/src/main.rs
@@ -1,6 +1,11 @@
 use sea_orm_migration::prelude::*;
 
+mod vec_to_json;
+
 #[async_std::main]
 async fn main() {
     cli::run_cli(migration::Migrator).await;
+
+    #[cfg(feature = "convert")]
+    vec_to_json::convert().await;
 }
