21677fdd27aebe915b1facb268abbe04b14d0683
Freeplay <freeplay@duck.com>
2023-06-13 15:16:46 -0400

    match posting form font size

--- packages/client/src/components/MkPostForm.vue
+++ packages/client/src/components/MkPostForm.vue
@@ -1193,7 +1193,7 @@ onMounted(() => {
 			padding: 0 24px;
 			margin: 0;
 			width: 100%;
-			font-size: 16px;
+			font-size: inherit;
 			border: none;
 			border-radius: 0;
 			background: transparent;
