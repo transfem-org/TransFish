084e9072d18d7231dd50cfaa4b4d1c35326f35f5
ThatOneCalculator <kainoa@t1c.dev>
2023-06-14 20:32:59 -0700

    fix: :ambulance: fix stream.ts

--- packages/client/src/stream.ts
+++ packages/client/src/stream.ts
@@ -3,26 +3,18 @@ import { markRaw } from "vue";
 import { $i } from "@/account";
 import { url } from "@/config";
 
-let stream: Misskey.Stream | null = null;
+export const stream = markRaw(
+	new Misskey.Stream(
+		url,
+		$i
+			? {
+					token: $i.token,
+			  }
+			: null,
+	),
+);
 
-export function useStream(): Misskey.Stream {
-	if (stream) return stream;
-
-	stream = markRaw(
-		new Misskey.Stream(
-			url,
-			$i
-				? {
-						token: $i.token,
-				  }
-				: null
-		)
-	);
-
-	window.setTimeout(heartbeat, 1000 * 60);
-
-	return stream;
-}
+window.setTimeout(heartbeat, 1000 * 60);
 
 function heartbeat(): void {
 	if (stream != null && document.visibilityState === "visible") {
