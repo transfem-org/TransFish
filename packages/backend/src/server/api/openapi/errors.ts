7ca519560f1b902101286a88990e682b01940572
ThatOneCalculator <kainoa@t1c.dev>
2023-06-22 17:14:27 -0700

    feat: :sparkles: display remaining time on ratelimits

--- packages/backend/src/server/api/openapi/errors.ts
+++ packages/backend/src/server/api/openapi/errors.ts
@@ -3,7 +3,7 @@ export const errors = {
 		INVALID_PARAM: {
 			value: {
 				error: {
-					message: "Invalid param.",
+					message: "Invalid parameter.",
 					code: "INVALID_PARAM",
 					id: "3d81ceae-475f-4600-b2a8-2bc116157532",
 				},
@@ -25,8 +25,7 @@ export const errors = {
 		AUTHENTICATION_FAILED: {
 			value: {
 				error: {
-					message:
-						"Authentication failed. Please ensure your token is correct.",
+					message: "Authentication failed.",
 					code: "AUTHENTICATION_FAILED",
 					id: "b0a7f5f8-dc2f-4171-b91f-de88ad238e14",
 				},
@@ -38,7 +37,7 @@ export const errors = {
 			value: {
 				error: {
 					message:
-						"You sent a request to Calc, Calckey's resident stoner furry, instead of the server.",
+						"You sent a request to Calc instead of the server. How did this happen?",
 					code: "I_AM_CALC",
 					id: "60c46cd1-f23a-46b1-bebe-5d2b73951a84",
 				},
