d0fd6a7de864e5b6859970bca8889bef840da2d7
ThatOneCalculator <kainoa@t1c.dev>
2023-06-23 17:55:42 -0700

    fix: :ambulance: disable lightningcss transformer for now

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -43,10 +43,6 @@ export default defineConfig(({ command, mode }) => {
 			}),
 		],
 
-		css: {
-			transformer: "lightningcss",
-		},
-
 		resolve: {
 			extensions,
 			alias: {
@@ -63,7 +59,7 @@ export default defineConfig(({ command, mode }) => {
 			),
 			_ENV_: JSON.stringify(process.env.NODE_ENV),
 			_DEV_: process.env.NODE_ENV !== "production",
-			_PERF_PREFIX_: JSON.stringify("Misskey:"),
+			_PERF_PREFIX_: JSON.stringify("Calckey:"),
 			_DATA_TRANSFER_DRIVE_FILE_: JSON.stringify("mk_drive_file"),
 			_DATA_TRANSFER_DRIVE_FOLDER_: JSON.stringify("mk_drive_folder"),
 			_DATA_TRANSFER_DECK_COLUMN_: JSON.stringify("mk_deck_column"),
