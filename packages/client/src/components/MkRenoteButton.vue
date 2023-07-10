ff7df3cc01bc9c20a66092f56023722e0d960253
freeplay <freeplay@duck.com>
2023-07-10 17:43:02 -0400

    style: use muted repeat icon instead of forbidden for disabled boosts

--- packages/client/src/components/MkRenoteButton.vue
+++ packages/client/src/components/MkRenoteButton.vue
@@ -10,8 +10,13 @@
 		<i class="ph-repeat ph-bold ph-lg"></i>
 		<p v-if="count > 0 && !detailedView" class="count">{{ count }}</p>
 	</button>
-	<button v-else class="eddddedb _button">
-		<i class="ph-prohibit ph-bold ph-lg"></i>
+	<button
+		v-else
+		class="eddddedb _button"
+		disabled="true"
+		v-tooltip.noDelay.bottom="i18n.ts.disabled"
+	>
+		<i class="ph-repeat ph-bold ph-lg"></i>
 	</button>
 </template>
 
