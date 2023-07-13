ea767909978b5dd7158c0d38a1a1f6053772e814
ThatOneCalculator <kainoa@t1c.dev>
2023-07-12 23:08:41 -0700

    refactor: :recycle: simplify shift key logic

--- packages/client/src/components/MkEmojiPickerDialog.vue
+++ packages/client/src/components/MkEmojiPickerDialog.vue
@@ -31,7 +31,7 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted, onBeforeUnmount } from "vue";
+import { ref } from "vue";
 import MkModal from "@/components/MkModal.vue";
 import MkEmojiPicker from "@/components/MkEmojiPicker.vue";
 import { defaultStore } from "@/store";
@@ -58,22 +58,9 @@ const emit = defineEmits<{
 
 const modal = ref<InstanceType<typeof MkModal>>();
 const picker = ref<InstanceType<typeof MkEmojiPicker>>();
-const isShiftKeyPressed = ref(false);
-
-const keydownHandler = (e) => {
-	if (e.key === "Shift") {
-		isShiftKeyPressed.value = true;
-	}
-};
-
-const keyupHandler = (e) => {
-	if (e.key === "Shift") {
-		isShiftKeyPressed.value = false;
-	}
-};
 
 function checkForShift(ev?: MouseEvent) {
-	if (!isShiftKeyPressed.value) {
+	if (ev?.shiftKey) {
 		modal.value?.close(ev);
 	}
 }
@@ -91,16 +78,6 @@ function opening() {
 	}
 	picker.value?.focus();
 }
-
-onMounted(() => {
-	window.addEventListener("keydown", keydownHandler);
-	window.addEventListener("keyup", keyupHandler);
-});
-
-onBeforeUnmount(() => {
-	window.removeEventListener("keydown", keydownHandler);
-	window.removeEventListener("keyup", keyupHandler);
-});
 </script>
 
 <style lang="scss" scoped>
