bd57b95a97098c72a1cb24891f1a3d0ea6086dfc
ThatOneCalculator <kainoa@t1c.dev>
2023-07-15 15:32:39 -0700

    docs: :bulb: going past DB_MAX_NOTE_TEXT_LENGTH

--- packages/backend/src/misc/hard-limits.ts
+++ packages/backend/src/misc/hard-limits.ts
@@ -3,6 +3,11 @@
 /**
  * Maximum note text length that can be stored in DB.
  * Surrogate pairs count as one
+ *
+ * NOTE: this can hypothetically be pushed further
+ * (up to 250000000), but will likely cause truncations
+ * and incompatibilities with other servers,
+ * as well as potential performance issues.
  */
 export const DB_MAX_NOTE_TEXT_LENGTH = 100000;
 
