45186be27707640e2512f5427e81c1e2bb169ee2
Johann150 <johann.galle@protonmail.com>
2023-08-03 08:12:49 -0700

    perf: :zap: seperate web and queue workers

--- packages/backend/src/boot/master.ts
+++ packages/backend/src/boot/master.ts
@@ -117,7 +117,7 @@ export async function masterMain() {
 	bootLogger.succ("Firefish initialized");
 
 	if (!envOption.disableClustering) {
-		await spawnWorkers(config.clusterLimit);
+		await spawnWorkers(config.clusterLimits);
 	}
 
 	bootLogger.succ(
@@ -126,7 +126,11 @@ export async function masterMain() {
 		true,
 	);
 
-	if (!envOption.noDaemons && !config.onlyQueueProcessor) {
+	if (
+		!envOption.noDaemons &&
+		config.clusterLimits?.web &&
+		config.clusterLimits?.web >= 1
+	) {
 		import("../daemons/server-stats.js").then((x) => x.default());
 		import("../daemons/queue-stats.js").then((x) => x.default());
 		import("../daemons/janitor.js").then((x) => x.default());
@@ -200,16 +204,30 @@ async function connectDb(): Promise<void> {
 	}
 }
 
-async function spawnWorkers(limit = 1) {
-	const workers = Math.min(limit, os.cpus().length);
-	bootLogger.info(`Starting ${workers} worker${workers === 1 ? "" : "s"}...`);
-	await Promise.all([...Array(workers)].map(spawnWorker));
+async function spawnWorkers(
+	clusterLimits: Required<Config["clusterLimits"]>,
+): Promise<void> {
+	const modes = ["web", "queue"];
+	const cpus = os.cpus().length;
+	for (const mode of modes.filter((mode) => clusterLimits[mode] > cpus)) {
+		bootLogger.warn(
+			`configuration warning: cluster limit for ${mode} exceeds number of cores (${cpus})`,
+		);
+	}
+
+	const total = modes.reduce((acc, mode) => acc + clusterLimits[mode], 0);
+	const workers = new Array(total);
+	workers.fill("web", 0, clusterLimits.web);
+	workers.fill("queue", clusterLimits.web);
+
+	bootLogger.info(`Starting ${total} workers...`);
+	await Promise.all(workers.map((mode) => spawnWorker(mode)));
 	bootLogger.succ("All workers started");
 }
 
-function spawnWorker(): Promise<void> {
+function spawnWorker(mode: "web" | "queue"): Promise<void> {
 	return new Promise((res) => {
-		const worker = cluster.fork();
+		const worker = cluster.fork({ mode });
 		worker.on("message", (message) => {
 			if (message === "listenFailed") {
 				bootLogger.error("The server Listen failed due to the previous error.");
