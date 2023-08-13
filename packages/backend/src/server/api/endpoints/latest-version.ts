7b9f0f36872e7fa8999fc4aaeee7ae7a6c67aad8
Namekuji <nmkj@waah.day>
2023-08-13 09:06:00 -0400

    chore: codeberg → gitlab

--- packages/backend/src/server/api/endpoints/latest-version.ts
+++ packages/backend/src/server/api/endpoints/latest-version.ts
@@ -15,9 +15,7 @@ export const paramDef = {
 
 export default define(meta, paramDef, async () => {
 	let tag_name;
-	await fetch(
-		"https://codeberg.org/api/v1/repos/firefish/firefish/releases?draft=false&pre-release=false&page=1&limit=1",
-	)
+	await fetch("https://git.joinfirefish.org/api/v4/projects/7/releases")
 		.then((response) => response.json())
 		.then((data) => {
 			tag_name = data[0].tag_name;
